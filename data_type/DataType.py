bdef153eba8bb2615c1893c3e1715f19b8cf7377
moocstudent <343840681@qq.com>
2020-11-23 17:04:26 +0800

    Initial commit

---
+++ data_type/DataType.py
@@ -0,0 +1,45 @@
+
+print(type(213))
+
+print(type(123.0))
+
+print(type('123'))
+
+print(type("123"))
+
+# Is that number of int type?
+print(isinstance(123,int))
+
+print(isinstance(123.0,float))
+
+print(isinstance('123',str))
+
+# Python 输出的数值要多于java
+print(10**1000)
+
+# 0b开头是2进制
+print(0b10101010101)
+
+# 0o开头8进制
+print(0o12345670)
+
+# 0x开头16进制
+print(0xdb273dc)
+print(0xDB273DC)
+
+print(1/2)
+print(1/3)
+print(1/6)
+
+print(type(1+1j))
+
+# 获取真值,这里并不是天文学里的超大数字的真值,而是真的值
+print((2.46+1.37j).real)
+
+# 复数虚部 这里并没有输出j
+print((2.46+1.37j).imag)
+
+print(123==123.0)
+
+print(123=='123')
+
